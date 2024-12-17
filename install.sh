f1176f7153591af775dfd39ee3b085568a67df0e
Kalle Hallivuori <kato@iki.fi>
2024-12-17 19:54:11 +0200

    create .config/autostart and put desktop files there

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 
 cd $(dirname "$BASH_SOURCE")
 
+mkdir -p ~/.config/autostart
+cp *.desktop ~/.config/autostart/
 kpackagetool5 --type=KWin/Script -i resize-window-to-3600x1920
 kwriteconfig5 --file kwinrc --group Plugins --key resizeTo3600x1920x0y0Enabled true
 qdbus org.kde.KWin /KWin reconfigure
